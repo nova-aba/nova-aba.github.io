3b9d6aa03fd09c7aa07e219fbb83d70d4df9d5e6
dhong42 <dhong42@gmail.com>
2016-08-17 16:18:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Author: Darren Hong
 function updateLineHeight()
 {
-	var attribution = "<div style='position: absolute; left: 5; bottom: 5; font-family: \
+	/*var attribution = "<div style='position: absolute; left: 5; bottom: 5; font-family: \
 	monospace; font-weight: bold; font-size: 10;'>Site designed by <a href='https://github.com/dhong42' \
 	target='_blank' style='text-decoration:underline;'>dhong42</a></div>"
-	document.body.innerHTML += attribution;
+	document.body.innerHTML += attribution;*/
 	if ((document.getElementById('contentline')!=null)) {
 		var line = document.getElementById('contentline');
 		line.style.top = document.getElementById('contentleft').offsetHeight/2 - 30 + 'px';
